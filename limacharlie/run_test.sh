678b684152ce74e46d78af36aaa57963f9165a22
Francis Mireault <mireaulf@gmail.com>
2021-02-18 16:55:31 -0500

    Run docker container after build

--- limacharlie/run_test.sh
+++ limacharlie/run_test.sh
@@ -2,4 +2,5 @@
 
 go test -c .
 
-sudo docker build --rm -f Dockerfile .
+sudo docker build -t limacharlie_tests -f Dockerfile .
+sudo docker run -e _OID=$LC_TEST_OID -e _KEY=$LC_TEST_KEY limacharlie_tests:latest
