6317d54eeb367c5a2614b8eaecdf4bc5ed6a0849
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2021-02-23 11:23:51 -0800

    Early Sync tests.

---
+++ limacharlie/sync_test.go
@@ -0,0 +1,55 @@
+package limacharlie
+
+import (
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v2"
+	"testing"
+)
+
+func TestSyncPush(t *testing.T) {
+	a := assert.New(t)
+	org := getTestOrgFromEnv(a)
+	rules, err := org.DRRules()
+	a.NoError(err)
+	if len(rules) != 0 {
+		t.Errorf("unexpected preexisting rules in add/delete: %+v", rules)
+	}
+
+	yc := `
+rules:
+  r1:
+    op: is
+    event: NEW_PROCESS
+    path: event/FILE_PATH
+    value: nope1
+  r2:
+    op: is
+    event: NEW_PROCESS
+    path: event/FILE_PATH
+    value: nope2
+  r3:
+    namespace: managed
+    op: is
+    event: NEW_PROCESS
+    path: event/FILE_PATH
+    value: nope3
+`
+	c := OrgConfig{}
+	if err := yaml.Unmarshal([]byte(yc), c); err != nil {
+		t.Errorf("yaml.Unmarshal: %v", err)
+	}
+
+	if len(c.DRRules) != 3 {
+		t.Errorf("unexpected conf: %+v", c)
+	}
+
+	ops, err := org.SyncPush(c, SyncOptions{
+		IsDryRun:    true,
+		SyncDRRules: true,
+	})
+	a.NoError(err)
+
+	if len(ops) != 3 {
+		t.Errorf("unexpected ops: %+v", err)
+	}
+}
