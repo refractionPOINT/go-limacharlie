f10e63ee325f47842247ce6311edb28a73ebdc73
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2021-04-03 11:58:38 -0700

    Add more tests.

--- limacharlie/sensor_test.go
+++ limacharlie/sensor_test.go
@@ -10,11 +10,68 @@ func TestSensorInfo(t *testing.T) {
 	a := assert.New(t)
 	org := getTestOrgFromEnv(a)
 
-	s, err := org.ListSensors()
+	// List all sensors.
+	sensors, err := org.ListSensors()
 	if err != nil {
 		t.Errorf("ListSensors: %v", err)
 	}
-	if len(s) == 0 {
+	if len(sensors) == 0 {
 		t.Error("no sensors listed")
+		return
+	}
+	sid := ""
+	for _, s := range sensors {
+		sid = s.SID
+		if s.Hostname == "" {
+			t.Errorf("missing sensor info: %+v", s)
+		}
+		if s.OID == "" {
+			t.Errorf("missing sensor info: %+v", s)
+		}
+		if s.IID == "" {
+			t.Errorf("missing sensor info: %+v", s)
+		}
+		if s.SID == "" {
+			t.Errorf("missing sensor info: %+v", s)
+		}
+		if s.Platform == 0 {
+			t.Errorf("missing sensor info: %+v", s)
+		}
+		if s.Architecture == 0 {
+			t.Errorf("missing sensor info: %+v", s)
+		}
+		if s.Organization == nil {
+			t.Errorf("missing sensor info: %+v", s)
+		}
+		if s.LastError != nil {
+			t.Errorf("missing sensor info: %+v", s)
+		}
+	}
+
+	// Get a single sensor.
+	s := org.GetSensor(sid).Update()
+	if s.Hostname == "" {
+		t.Errorf("missing sensor info: %+v", s)
+	}
+	if s.OID == "" {
+		t.Errorf("missing sensor info: %+v", s)
+	}
+	if s.IID == "" {
+		t.Errorf("missing sensor info: %+v", s)
+	}
+	if s.SID == "" {
+		t.Errorf("missing sensor info: %+v", s)
+	}
+	if s.Platform == 0 {
+		t.Errorf("missing sensor info: %+v", s)
+	}
+	if s.Architecture == 0 {
+		t.Errorf("missing sensor info: %+v", s)
+	}
+	if s.Organization == nil {
+		t.Errorf("missing sensor info: %+v", s)
+	}
+	if s.LastError != nil {
+		t.Errorf("missing sensor info: %+v", s)
 	}
 }
