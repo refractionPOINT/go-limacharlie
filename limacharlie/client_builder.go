986abdcfb887fd656f032c53da092045f2a786e4
Francis Mireault <mireaulf@gmail.com>
2021-04-30 15:17:08 -0400

    Add init for client builder

--- limacharlie/client_builder.go
+++ limacharlie/client_builder.go
@@ -11,6 +11,13 @@ type ClientBuilder struct {
 
 var _ LCClientBuilder = &ClientBuilder{}
 
+func NewClientBuilder(logger LCLogger, clientLoaders []ClientOptionLoader) *ClientBuilder {
+	return &ClientBuilder{
+		logger:        logger,
+		clientLoaders: clientLoaders,
+	}
+}
+
 func (b *ClientBuilder) NewClient(opt ClientOptions) (LCClient, error) {
 	return NewClientFromLoader(opt, b.logger, b.clientLoaders...)
 }
