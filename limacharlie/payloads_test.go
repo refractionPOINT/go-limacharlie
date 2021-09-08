cb6fa2d683123029086916e8c4a2752579ec7810
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2021-09-08 10:47:25 -0700

    Adding tests

--- limacharlie/payloads_test.go
+++ limacharlie/payloads_test.go
@@ -1,1 +1,49 @@
 package limacharlie
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPayloads(t *testing.T) {
+	a := assert.New(t)
+	org := getTestOrgFromEnv(a)
+	testName := uuid.NewString()
+	testData := []byte("thisisatestpayload")
+
+	// Create a new Payload.
+	err := org.CreatePayloadFromBytes(testName, testData)
+	a.NoError(err)
+
+	// Verify it is there.
+	payloads, err := org.Payloads()
+	a.NoError(err)
+	entry, ok := payloads[testName]
+	a.Equal(ok, true)
+	a.Equal(entry.Name, testName)
+	a.Equal(entry.Oid, org.client.options.OID)
+	a.Equal(entry.Size, len(testData))
+
+	// Try to re-create it to get an error.
+	err = org.CreatePayloadFromBytes(testName, testData)
+	a.Error(err, "adding a payload with the same name should raise an error: %s", err)
+
+	// Get the payload data.
+	data, err := org.Payload(testName)
+	a.NoError(err)
+	a.NotEqual(data, nil)
+	a.Equal(string(data), string(testData))
+
+	// Delete the payload.
+	err = org.DeletrePayload(testName)
+	a.NoError(err)
+
+	// Make sure it is deleted.
+	payloads, err = org.Payloads()
+	a.NoError(err)
+	_, ok = payloads[testName]
+	a.Equal(ok, false)
+}
