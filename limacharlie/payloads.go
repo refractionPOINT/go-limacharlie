344868e3454e1385209bf17ecae7d58881e52b5f
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2021-09-08 11:33:23 -0700

    Add payloads (#50)
    * First code draft
    
    * Adding tests
    
    * Payload is singular in URL.
    
    * Only run our test for now to accelerate tests.
    
    * Wrong verb
    
    * Log existing payloads.
    
    * Fix payload list format.
    
    * Fix test datatype
    
    * Run all tests
    
    * Update limacharlie/payloads.go
    
    Co-authored-by: Francis Mireault <mireaulf@gmail.com>
    
    Co-authored-by: Francis Mireault <mireaulf@gmail.com>

---
+++ limacharlie/payloads.go
@@ -0,0 +1,88 @@
+package limacharlie
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Payload struct {
+	Name      string `json:"name"`
+	Oid       string `json:"oid"`
+	Size      uint64 `json:"size"`
+	By        string `json:"by"`
+	CreatedOn uint64 `json:"created"`
+}
+type payloadsList struct {
+	Payloads map[PayloadName]Payload `json:"payloads"`
+}
+type PayloadName = string
+type payloadGetPointer struct {
+	URL string `json:"get_url"`
+}
+type payloadPutPointer struct {
+	URL string `json:"put_url"`
+}
+
+// List all the Payloads in an LC organization.
+func (org Organization) Payloads() (map[PayloadName]Payload, error) {
+	resp := payloadsList{}
+	request := makeDefaultRequest(&resp)
+	if err := org.client.reliableRequest(http.MethodGet, fmt.Sprintf("payload/%s", org.client.options.OID), request); err != nil {
+		return nil, err
+	}
+	return resp.Payloads, nil
+}
+
+// Download the content of a Payload in an LC organization.
+func (org Organization) Payload(name PayloadName) ([]byte, error) {
+	resp := payloadGetPointer{}
+	request := makeDefaultRequest(&resp)
+	if err := org.client.reliableRequest(http.MethodGet, fmt.Sprintf("payload/%s/%s", org.client.options.OID, name), request); err != nil {
+		return nil, err
+	}
+	httpResp, err := http.Get(resp.URL)
+	if err != nil {
+		return nil, err
+	}
+	defer httpResp.Body.Close()
+	data, err := ioutil.ReadAll(httpResp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+// Delete a Payload from within an LC organization.
+func (org Organization) DeletePayload(name PayloadName) error {
+	resp := Dict{}
+	request := makeDefaultRequest(&resp)
+	if err := org.client.reliableRequest(http.MethodDelete, fmt.Sprintf("payload/%s/%s", org.client.options.OID, name), request); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Create a Payload in an LC organization.
+func (org Organization) CreatePayloadFromBytes(name PayloadName, data []byte) error {
+	resp := payloadPutPointer{}
+	request := makeDefaultRequest(&resp)
+	if err := org.client.reliableRequest(http.MethodPost, fmt.Sprintf("payload/%s/%s", org.client.options.OID, name), request); err != nil {
+		return err
+	}
+	c := &http.Client{}
+	req, err := http.NewRequest(http.MethodPut, resp.URL, bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/octet-stream")
+	httpResp, err := c.Do(req)
+	if err != nil {
+		return err
+	}
+	if httpResp.StatusCode != 200 {
+		return fmt.Errorf("failed to PUT payload, http status: %d", httpResp.StatusCode)
+	}
+	return nil
+}
