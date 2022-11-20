4d716946381c864377dd008bedb4a75fc7cdc1ed
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2022-11-19 16:15:32 -0800

    Merge objects test using insight into one.

--- limacharlie/insight_objects_test.go
+++ limacharlie/insight_objects_test.go
@@ -23,35 +23,17 @@ func TestInsightObjects(t *testing.T) {
 	a.NoError(err)
 	a.Equal("%google%", resp.IndicatorName)
 	a.Equal(InsightObjectTypes.Domain, resp.ObjectType)
-}
 
-func TestInsightObjectsPerObject(t *testing.T) {
-	a := assert.New(t)
-	org := getTestOrgFromEnv(a)
-	unsubReplicantCB, err := findUnsubscribeCallback(org, "api", "insight")
-	a.NoError(err)
-	if unsubReplicantCB != nil {
-		defer unsubReplicantCB()
-	}
-	resp, err := org.InsightObjectsPerObject(InsightObjectsRequest{
+	respPO, err := org.InsightObjectsPerObject(InsightObjectsRequest{
 		IndicatorName:  "%google%",
 		ObjectType:     InsightObjectTypes.Domain,
 		ObjectTypeInfo: InsightObjectTypeInfoTypes.Summary,
 		AllowWildcards: true,
 	})
 	a.NoError(err)
-	a.Equal("%google%", resp.IndicatorName)
-	a.Equal(InsightObjectTypes.Domain, resp.ObjectType)
-}
+	a.Equal("%google%", respPO.IndicatorName)
+	a.Equal(InsightObjectTypes.Domain, respPO.ObjectType)
 
-func TestInsightObjectsBatch(t *testing.T) {
-	a := assert.New(t)
-	org := getTestOrgFromEnv(a)
-	unsubReplicantCB, err := findUnsubscribeCallback(org, "api", "insight")
-	a.NoError(err)
-	if unsubReplicantCB != nil {
-		defer unsubReplicantCB()
-	}
 	_, err = org.InsightObjectsBatch(InsightObjectsBatchRequest{
 		Objects: map[InsightObjectType][]string{
 			InsightObjectTypes.Domain: {"google.com", "microsoft.com"},
