8bf6aceaac2288dc91f664c4d353b265836e2b4e
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2022-12-31 11:40:42 -0800

    Add a GCP logger. (#97)

--- limacharlie/logger.go
+++ limacharlie/logger.go
@@ -1,6 +1,15 @@
 package limacharlie
 
-import "github.com/rs/zerolog"
+import (
+	"encoding/json"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/rs/zerolog"
+
+	"github.com/google/uuid"
+)
 
 // LCLogger is the interface for limacharlie logging
 type LCLogger interface {
@@ -66,4 +75,94 @@ func (l *LCLoggerZerolog) Debug(msg string) {
 // Trace see zerolog logger trace function
 func (l *LCLoggerZerolog) Trace(msg string) {
 	l.l.Trace().Msg(msg)
+}
+
+// LCLoggerGCP implements the logging interface with GCP structured logging
+type gcpLogTime struct {
+	Seconds int64 `json:"seconds"`
+	Nanos   int   `json:"nanos"`
+}
+
+type gcpLogLine struct {
+	Message    string                 `json:"message"`
+	Timestamp  gcpLogTime             `json:"timestamp"`
+	CustomData map[string]interface{} `json:"ext_data,omitempty"`
+	InstanceID string                 `json:"instance_id,omitempty"`
+	Severity   string                 `json:"severity,omitempty"`
+}
+
+type LCLoggerGCP struct {
+	m          sync.Mutex
+	instanceID string
+	once       sync.Once
+}
+
+func (l *LCLoggerGCP) logToStdout(msg string, severity string) {
+	line := gcpLogLine{
+		Message:    string(msg),
+		InstanceID: l.instanceID,
+		Severity:   severity,
+	}
+	now := time.Now()
+	line.Timestamp.Seconds = now.Unix()
+	line.Timestamp.Nanos = now.Nanosecond()
+	b, _ := json.Marshal(&line)
+	l.m.Lock()
+	defer l.m.Unlock()
+	os.Stdout.Write(b)
+}
+
+func (l *LCLoggerGCP) logToStderr(msg string, severity string) {
+	line := gcpLogLine{
+		Message:    string(msg),
+		InstanceID: l.instanceID,
+		Severity:   severity,
+	}
+	now := time.Now()
+	line.Timestamp.Seconds = now.Unix()
+	line.Timestamp.Nanos = now.Nanosecond()
+	b, _ := json.Marshal(&line)
+	l.m.Lock()
+	defer l.m.Unlock()
+	os.Stderr.Write(b)
+}
+
+func (l *LCLoggerGCP) init() {
+	l.instanceID = uuid.NewString()
+}
+
+// Fatal see GCP logger fatal function
+func (l *LCLoggerGCP) Fatal(msg string) {
+	l.once.Do(l.init)
+	l.logToStderr(msg, "CRITICAL")
+}
+
+// Error see GCP logger error function
+func (l *LCLoggerGCP) Error(msg string) {
+	l.once.Do(l.init)
+	l.logToStderr(msg, "ERROR")
+}
+
+// Warn see GCP logger warn function
+func (l *LCLoggerGCP) Warn(msg string) {
+	l.once.Do(l.init)
+	l.logToStdout(msg, "WARNING")
+}
+
+// Info see GCP logger info function
+func (l *LCLoggerGCP) Info(msg string) {
+	l.once.Do(l.init)
+	l.logToStdout(msg, "INFO")
+}
+
+// Debug see GCP logger debug function
+func (l *LCLoggerGCP) Debug(msg string) {
+	l.once.Do(l.init)
+	l.logToStdout(msg, "DEBUG")
+}
+
+// Trace see GCP logger trace function
+func (l *LCLoggerGCP) Trace(msg string) {
+	l.once.Do(l.init)
+	l.logToStdout(msg, "DEFAULT")
 }
