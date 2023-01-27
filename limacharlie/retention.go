aab77bc843dff96e4f0b6b40c94147d9a9f8ad43
Edinson Yosa <eyosa@soteria.io>
2023-01-27 11:16:00 -0500

    Get Event by atom
    Adds a struct to get an event by atom and the sensor ID

--- limacharlie/retention.go
+++ limacharlie/retention.go
@@ -11,6 +11,32 @@ type Stats struct {
 type DetStats struct {
 	Totals map[string]map[string]int `json:"totals"`
 }
+type EventContainer struct {
+	Event Event `json:"event"`
+}
+type Event struct {
+	Event     interface{} `json:"event"`
+	Routing   Routing     `json:"routing"`
+	TimeStamp string      `json:"ts"`
+}
+type Routing struct {
+	Arch      int      `json:"arch"`
+	DID       string   `json:"did"`
+	EventID   string   `json:"event_id"`
+	EventTime int64    `json:"event_time"`
+	EventType string   `json:"event_type"`
+	ExtIP     string   `json:"ext_ip"`
+	Hostname  string   `json:"hostname"`
+	IID       string   `json:"iid"`
+	IntIP     string   `json:"int_ip"`
+	ModuleID  int      `json:"moduleid"`
+	OID       string   `json:"oid"`
+	Parent    string   `json:"parent"`
+	Plat      int      `json:"plat"`
+	SID       string   `json:"sid"`
+	Tags      []string `json:"tags"`
+	This      string   `json:"this"`
+}
 
 func (org *Organization) OnlineStats(start int64, end int64) (Stats, error) {
 	stats := Stats{}
@@ -49,4 +75,11 @@ func (org *Organization) DetectionStats(start int64, end int64) (DetStats, error
 		return stats, err
 	}
 	return stats, nil
+}
+
+func (org *Organization) EventByAtom(sensorID, atom string) (EventContainer, error) {
+	event := EventContainer{}
+	q := makeDefaultRequest(&event)
+	err := org.client.reliableRequest(http.MethodGet, fmt.Sprintf("insight/%s/%s/%s", org.client.options.OID, sensorID, atom), q)
+	return event, err
 }
