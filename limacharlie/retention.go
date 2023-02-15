5a52c0a4c6d1b57d4057bc682d17e82181a98aea
Aarush Ahuja <ahujaarush@gmail.com>
2023-02-15 21:46:10 +0530

    feat: add method to fetch historical detections for organizations (#106)
    * feat: add method to fetch historical detections for organizations
    
    * refactor: keep detect struct generic
    
    Co-authored-by: Maxime Lamothe-Brassard <maximelb@google.com>
    
    ---------
    
    Co-authored-by: Maxime Lamothe-Brassard <maximelb@google.com>

--- limacharlie/retention.go
+++ limacharlie/retention.go
@@ -89,3 +89,66 @@ func (org *Organization) EventByAtom(sensorID, atom string) (EventContainer, err
 	err := org.client.reliableRequest(http.MethodGet, fmt.Sprintf("insight/%s/%s/%s", org.client.options.OID, sensorID, atom), q)
 	return event, err
 }
+
+type HistoricalDetectionsRequest struct {
+	// Cat is the category of the detections to fetch
+	Cat string `json:"cat"`
+	// Cursor is optional for paginated access, set to '-' for first query
+	Cursor string `json:"cursor"`
+	// Start is the required timestamp in seconds where to stop fetching detections
+	Start int `json:"start"`
+	// End is the required timestamp in seconds where to stop fetching detections
+	End int `json:"end"`
+	// Limit maximum number of detections to return
+	Limit int `json:"limit"`
+}
+
+func (org Organization) HistoricalDetections(detectionReq HistoricalDetectionsRequest) (HistoricalDetectionsResponse, error) {
+
+	var results HistoricalDetectionsResponse
+
+	if detectionReq.Cursor == "" {
+		detectionReq.Cursor = "-"
+	}
+
+	q := makeDefaultRequest(&results)
+	q = q.withQueryData(detectionReq)
+
+	if err := org.client.reliableRequest(http.MethodGet, fmt.Sprintf("insight/%s/detections", org.client.options.OID), q); err != nil {
+		return results, err
+	}
+
+	return results, nil
+}
+
+type HistoricalDetectionsResponse struct {
+	Detects    []Detects `json:"detects"`
+	NextCursor string    `json:"next_cursor"`
+}
+
+type Detect struct {
+	Event   Event   `json:"event"`
+	Routing Routing `json:"routing"`
+}
+type DetectMetadata struct {
+	Author         string   `json:"author"`
+	Description    string   `json:"description"`
+	Falsepositives []string `json:"falsepositives"`
+	Level          string   `json:"level"`
+	References     []string `json:"references"`
+	Tags           []string `json:"tags"`
+}
+
+type Detects struct {
+	Author    string         `json:"author"`
+	Cat       string         `json:"cat"`
+	Detect    Dict         `json:"detect"`
+	DetectID  string         `json:"detect_id"`
+	DetectMtd DetectMetadata `json:"detect_mtd"`
+	Link      string         `json:"link"`
+	Namespace string         `json:"namespace"`
+	Routing   Routing        `json:"routing"`
+	Source    string         `json:"source"`
+	Ts        int64          `json:"ts"`
+}
+
