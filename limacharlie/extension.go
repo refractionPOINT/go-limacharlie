72da58be9a71463717dc926374d7abf01d16fa0b
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2023-08-01 09:34:12 -0700

    Fix timeouts for sub/unsub of extensions.

--- limacharlie/extension.go
+++ limacharlie/extension.go
@@ -3,6 +3,7 @@ package limacharlie
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 type ExtensionName = string
@@ -25,7 +26,7 @@ func (org Organization) Extensions() ([]ExtensionName, error) {
 func (org Organization) SubscribeToExtension(name ExtensionName) error {
 	d := Dict{}
 	if err := org.client.reliableRequest(http.MethodPost,
-		fmt.Sprintf("orgs/%s/subscription/extension/%s", org.client.options.OID, name), makeDefaultRequest(&d)); err != nil {
+		fmt.Sprintf("orgs/%s/subscription/extension/%s", org.client.options.OID, name), makeDefaultRequest(&d).withTimeout(1*time.Minute)); err != nil {
 		return err
 	}
 	return nil
@@ -34,7 +35,7 @@ func (org Organization) SubscribeToExtension(name ExtensionName) error {
 func (org Organization) UnsubscribeFromExtension(name ExtensionName) error {
 	d := Dict{}
 	if err := org.client.reliableRequest(http.MethodDelete,
-		fmt.Sprintf("orgs/%s/subscription/extension/%s", org.client.options.OID, name), makeDefaultRequest(&d)); err != nil {
+		fmt.Sprintf("orgs/%s/subscription/extension/%s", org.client.options.OID, name), makeDefaultRequest(&d).withTimeout(1*time.Minute)); err != nil {
 		return err
 	}
 	return nil
