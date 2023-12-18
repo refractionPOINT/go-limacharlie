a5844e4d95778c7fc5a3bf8cb3481c195c40b59b
Joshua Padilla <joshua@refractionpoint.com>
2023-12-18 15:12:36 -0600

    commenting out test to troubleshoot payloads

--- limacharlie/payloads_test.go
+++ limacharlie/payloads_test.go
@@ -1,6 +1,7 @@
 package limacharlie
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/google/uuid"
@@ -12,6 +13,9 @@ func TestPayloads(t *testing.T) {
 	a := assert.New(t)
 	org := getTestOrgFromEnv(a)
 	unsubReplicantCB, err := findUnsubscribeCallback(org, "api", "insight")
+	if err != nil {
+		fmt.Printf("this is error for payload test  %v \n", err)
+	}
 	a.NoError(err)
 	if unsubReplicantCB != nil {
 		defer unsubReplicantCB()
