7181bd72f93756fd3187e77d8dcb4cd769980e3f
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2025-02-28 16:06:20 -0800

    Remove a ton of old test for features no longer supported.

--- limacharlie/artifact_test.go
+++ limacharlie/artifact_test.go
@@ -41,42 +41,6 @@ func TestArtifactUpload(t *testing.T) {
 	}
 }
 
-func TestLoggingAddDelete(t *testing.T) {
-	a := assert.New(t)
-	org := getTestOrgFromEnv(a)
-
-	unsubReplicantCB, err := findUnsubscribeReplicantCallback(org, "logging")
-	a.NoError(err)
-	if unsubReplicantCB != nil {
-		defer unsubReplicantCB()
-	}
-
-	artifactsRules, err := org.ArtifactsRules()
-	a.NoError(err)
-	artifactsRulesStartCount := len(artifactsRules)
-
-	a.NoError(org.ArtifactRuleAdd("test-rule", ArtifactRule{
-		IsIgnoreCert:   true,
-		IsDeleteAfter:  true,
-		DaysRetentions: 90,
-		Patterns:       []string{"/var/log.log", "/home/user"},
-		Filters: ArtifactRuleFilter{
-			Tags:      []string{"test-tag0"},
-			Platforms: []string{"windows", "chrome"},
-		},
-	}))
-
-	artifactsRules, err = org.ArtifactsRules()
-	a.NoError(err)
-	a.Equal(artifactsRulesStartCount+1, len(artifactsRules))
-
-	a.NoError(org.ArtifactRuleDelete("test-rule"))
-
-	artifactsRules, err = org.ArtifactsRules()
-	a.NoError(err)
-	a.Equal(artifactsRulesStartCount, len(artifactsRules))
-}
-
 // func TestArtifactExport(t *testing.T) {
 // 	o, err := NewOrganizationFromClientOptions(ClientOptions{
 // 		OID: "",
